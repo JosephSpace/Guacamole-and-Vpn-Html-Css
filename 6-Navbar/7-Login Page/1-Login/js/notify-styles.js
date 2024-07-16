6d87d6278c13f85c84241130bfab7b9da96cddea
JosephSpace <126154776+JosephSpace@users.noreply.github.com>
2024-07-16 20:43:38 +0300

    Add files via upload

---
+++ 6-Navbar/7-Login Page/1-Login/js/notify-styles.js
@@ -0,0 +1,62 @@
+$.notify.addStyle('Warning', {
+    html: "<div><p>  <span data-notify-text/>  </p></div>",
+    classes: {
+      base: {
+        "white-space": "nowrap",
+        "background-color": "#ff9e0f",
+        "color":"#fff",
+        "padding": "10px",
+        "text-align":"center",
+        "border-radius":"10px",
+        "font-size":"1.1em",
+        "height":"2.5em",
+        "globalPosition":'bottom left',
+        "showAnimation": 'slideDown',
+        "hideAnimation": 'slideUp',
+      },}});
+  $.notify.addStyle('Info', {
+    html: "<div class='card'><p><strong>Info!</strong>  <span data-notify-text/></p></div>",
+    classes: {
+      base: {
+        "white-space": "nowrap",
+        "background-color": "#00cae3",
+        "color":"#fff",
+        "padding": "10px",
+        "text-align":"center",
+        "border-radius":"10px",
+        "font-size":"1.1em",
+        "height":"2.5em",
+        "showAnimation": 'slideDown',
+        "hideAnimation": 'slideUp',
+      },}});
+  $.notify.addStyle('Danger', {
+    html: "<div><p>  <span data-notify-text/>  </p></div>",
+    classes: {
+      base: {
+        "white-space": "nowrap",
+        "background-color": "#f55145",
+        "color":"#fff",
+        "padding": "10px",
+        "text-align":"center",
+        "border-radius":"10px",
+        "font-size":"1.1em",
+        "height":"2.5em",
+        "showAnimation": 'slideDown',
+        "hideAnimation": 'slideUp',
+      },}});
+  $.notify.addStyle('Success', {
+    html: "<div><p><strong>Success!</strong>  <span data-notify-text/></p></div>",
+    classes: {
+      base: {
+        "white-space": "nowrap",
+        "background-color": "#55b559",
+        "color":"#fff",
+        "padding": "10px",
+        "text-align":"center",
+        "border-radius":"10px",
+        "font-size":"1.1em",
+        "height":"2.5em",
+        "showAnimation": 'slideDown',
+        "hideAnimation": 'slideUp',
+      },}});
+  
